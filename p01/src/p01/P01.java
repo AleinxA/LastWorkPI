21f0a82b2adc9048e82bcd83efa51eacacc2f870
Aleinx <aleinx.official@gmail.com>
2023-12-21 13:21:47 -0500

    Testing

--- p01/src/p01/P01.java
+++ p01/src/p01/P01.java
@@ -10,7 +10,9 @@ public class P01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hello World!!");
+        
     }
     
 }
