c5330107eb1e3155dab5d1a034f54d9b8a7b18db
AleinxA <129327087+AleinxA@users.noreply.github.com>
2023-12-21 12:55:19 -0500

    Add files via upload

---
+++ p01/src/p01/P01.java
@@ -0,0 +1,16 @@
+package p01;
+
+/**
+ *
+ * @author alein
+ */
+public class P01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
